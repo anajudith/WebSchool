6b94d16ed6a3dd637c046d997df5c7c2c06d93e7
Ana Judith <ana-judth@hotmail.com>
2021-11-24 21:58:41 -0300

    Resolvendo banco

--- server/banco.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO usuarios (nome, email) VALUES ( "Usuario2", "usuario2@gmail.com");
-
--- server/index.js
+++ server/index.js
@@ -1,34 +1,35 @@
-// const Sequelize = require('sequelize')
-// const sequelize = new Sequelize('webschool', 'root', 'trabalho', {
+//  const Sequelize = require('sequelize')
+//  const sequelize = new Sequelize('webschool', 'root', '123', {
 //     //qual maquina/servidor esta o banco
-//     host: "localhost",
-//     dialect: 'mysql'
-// }) //conectando ao banco de dados 
+//      host: "localhost",
+//      dialect: 'mysql'
+//  }) //conectando ao banco de dados 
 
-// sequelize.authenticate().then( function () {
-//     console.log("Conectado com sucesso!!!!!!!")
-// }).catch(function(erro) {
-//     console.log("Falha ao se conectar: "+erro)
-// });
+//  sequelize.authenticate().then( function () {
+//      console.log("Conectado com sucesso!!!!!!!")
+//  }).catch(function(erro) {
+//      console.log("Falha ao se conectar: "+erro)
+//  });
 
 
 const express = require("express");
 const app = express();
-const mysql = require('mysql');
+ const mysql = require('mysql');
 
-const db = mysql.createPool({
+ const db = mysql.createPool({
     host: "localhost",
     user : "root",
-    password : "123",
+    password : "1234",
     database: "webschool",
-});
+ });
 
 app.get("/", (req, res) => {
     db.query("INSERT INTO usuarios (nome, email, senha, Telefone, Data_de_ascimento, CPF, TipoUsuario) VALUES ('Fulano2', 'fulano@gmail.com', '123456', '99999999', '11/11/2011', '12345678911', 'P')", (err, result) => {
         if(err) {
-            console.log(err)
-        }
-    })
+             debugger
+             console.log(err)
+         }
+     })
 })
 
 app.listen(3001, () => {
