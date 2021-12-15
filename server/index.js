ceb3a5d4da551ab3426fc8cda028abc65e3e4f39
Ana Judith <ana.pereira@dtidigital.com.br>
2021-12-15 17:10:33 -0300

    server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const saltRounds = 10;
 
 const db = mysql.createPool({
@@ -57,6 +58,15 @@ app.post("/register", (req, res) => {
   });
 });
 
+app.get("/curso", (req, res) => {
+  db.query("SELECT * FROM curso", (err, result) => {
+    if (err) {
+      res.send(err);
+    }
+    res.send(result);
+  });
+});
+
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const senha = req.body.password;
@@ -80,6 +90,82 @@ app.post("/login", (req, res) => {
       res.send({ msg: "Conta não encontrada" });
     }
   });
+});
+
+app.post("/cadastrarCurso", (req, res) => {
+  const nomeCurso = req.body.nomeCurso;
+  const descricaoCurso = req.body.descricaoCurso;
+  const tempoDuracao = req.body.tempoDuracao;
+  const urlCurso = req.body.urlCurso;
+
+  const id = crypto.randomInt(1, 1000);
+
+  db.query(
+    "INSERT INTO curso (idCursos, nomeCurso, descricaoCurso, tempoDuracao, url) VALUES (?, ?, ?, ?, ?)",
+    [id, nomeCurso, descricaoCurso, tempoDuracao, urlCurso],
+    (err, response) => {
+      if (err) {
+        res.send(err);
+      }
+      if(response) {
+        res.send({ msg: "Curso inserido com sucesso", idCurso: id
+       });
+      } else {
+        res.send({ msg: "Não foi possivel inserir o curso" });
+      }
+    }
+  );
+});
+
+app.post("/cadastrarModulo", (req, res) => {
+  const tituloModulo = req.body.tituloModulo;
+  const tempoDuracao = req.body.tempoDuracao;
+  const idCurso = req.body.idCurso;
+
+  const id = crypto.randomInt(1, 1000);
+
+  db.query(
+    "INSERT INTO modulo (idModulo, tituloModulo, tempoDuracao, url, idCurso) VALUES (?, ?, ?, ?, ?)",
+    [id, tituloModulo, tempoDuracao, "", idCurso],
+    (err, response) => {
+      if (err) {
+        res.send(err);
+      }
+      if(response) {
+        res.send({ msg: "Modulo inserido para esse curso", idModulo: id
+       });
+      } else {
+        res.send({ msg: "Não foi possivel inserir o modulo" });
+      }
+    }
+  );
+});
+
+app.post("/cadastrarAula", (req, res) => {
+  const nomeAula = req.body.nomeAula;
+  const descricaoAula = req.body.descricaoAula;
+  const tempoDuracao = req.body.tempoDuracao;
+  const urlAula = req.body.urlAula;
+  const idModulo = req.body.idModulo;
+  
+
+  const id = crypto.randomInt(1, 1000);
+
+  db.query(
+    "INSERT INTO aula (idAula, nomeAula, descricaoAula, tempoDuracao, url, idModulo) VALUES (?, ?, ?, ?, ?,? )",
+    [id, nomeAula, descricaoAula, tempoDuracao, urlAula, idModulo],
+    (err, response) => {
+      if (err) {
+        res.send(err);
+      }
+      if(response) {
+        res.send({ msg: "Aula inserida"
+       });
+      } else {
+        res.send({ msg: "Não foi possivel inserir a aula" });
+      }
+    }
+  );
 });
 
 app.listen(3001, () => {
